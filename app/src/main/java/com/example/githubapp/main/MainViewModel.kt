93be39146f6817d46a78ff7ae66a06918daaf071
M0710Fa <b1018188.fun@gmail.com>
2022-09-20 10:45:18 +0900

    create viewmodel

---
+++ app/src/main/java/com/example/githubapp/main/MainViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.githubapp.main
+
+import android.accounts.NetworkErrorException
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.githubapp.model.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val userRepository: UserRepository
+): ViewModel() {
+    //Viewの状態を表すSealed Class
+    sealed class UiState{
+        //初期状態
+        object Initial: UiState()
+        //ローディング
+        object  Loading: UiState()
+        //読み込み成功
+        object  Success: UiState()
+        //読み込み失敗
+        object  Failure: UiState()
+    }
+
+    val uiState: MutableState<UiState> = mutableStateOf(UiState.Initial)
+
+    //検索フォームの入力文字
+    val searchQuery: MutableState<String> = mutableStateOf("")
+
+
+    fun onSearchTapped(){
+        val searchQuery = searchQuery.value
+
+        viewModelScope.launch {
+            uiState.value = UiState.Loading
+            try {
+                userRepository.getUser(userName = searchQuery)
+                uiState.value = UiState.Success
+            }catch (e: Exception){
+                uiState.value = UiState.Failure
+            }
+        }
+    }
+}
